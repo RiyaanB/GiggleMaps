9ed0264c35ea35b7bbf6d43f4181a15950cf73b9
twixupmysleeve <pratham.mehta001@gmail.com>
2021-03-26 19:51:08 +0530

    Added functionalities for Graph class and integrated in main()

--- distributedDjikstra.py
+++ distributedDjikstra.py
@@ -1,38 +1,48 @@
 # PSEUDOCODE:
 
+# TODO: Consider dividing each edge into a bunch of sub graphs
+# 	to accommodate for multiple people being on 1 road
+
+
+from collections import defaultdict
 
 def main():
 	graph = Graph()
 
 	people = 3
 
-	people = [Person() for i in range(people)] #Change range value to change number of ppl
 	start = 'A'
 	end = 'F'
 
+	people = [Person(start, end) for i in range(people)] #Change range value to change number of ppl
+
 	while people:
 		for person in people:
-			if not person.reached(end):
+			if not person.reached():
 				route = dijkstra(person.current_pos, end)
 				next_pos = route[0]
-				graph.update_cost(person.current_pos, next_pos) # increases cost of the edge on which person travels
+				if not person.current_pos==start:
+					graph.update_cost(prev_pos, person.current_pos, value=-1) # reduces cost of the edge the person is no longer on
+
+				graph.update_cost(person.current_pos, next_pos, value=1) # increases cost of the edge on which person travels
+				prev_pos = person.current_pos
 				person.move(next_pos)
 
 			else:
 				people.remove(person)
 
 class Person:
-	# I think you should add 2 arguments - Start and end here and then note those values
-	def __init__(self):
+	
+	def __init__(self, start=None, end=None):
 		self.current_pos = None
 		
-		self.start = None
-		self.end = None
+		self.start = start
+		self.end = end
 
 		self.route_taken = [self.start]
 
-	def reached(self, end):
-		return self.current_pos==end
+	def reached(self):
+		return self.current_pos==self.end
 
 	def move(self, pos):
 		'''
@@ -47,10 +57,22 @@ class Person:
 
 
 class Graph:
-	def __init__(self):
-		pass
+	def __init__(self, filename):
+		self.nodes = set()
+		self.edges = defaultdict(dict)
 
-	def update_cost(self):
+		with open(filename) as f:
+			for line in f:
+				from_node, to_node, cost, *_ = line.strip().split(" ")
+				cost = int(cost)
+				self.edges[from_node][to_node] = cost
+				self.edges[to_node][from_node] = cost
+				self.nodes.add(from_node)
+				self.nodes.add(to_node)
+
+	def update_cost(self, initial, final, value=1):
+		self.edges[initial][final] += value
+		self.edges[final][initial] += value
 		pass
 
 
