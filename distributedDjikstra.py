3714432d9c035e4dd26b5cfb28b958e99fedbf4a
NFJ1618 <67336232+NFJ1618@users.noreply.github.com>
2021-03-27 18:43:16 +0530

    Update distributedDjikstra.py

--- distributedDjikstra.py
+++ distributedDjikstra.py
@@ -22,7 +22,7 @@ def main():
 	while people:
 		for person in people:
 			if not person.reached():
-				route = dijkstra(person.current_pos, person.end)
+				route = dijkstra(graph, person.current_pos, person.end)
 				next_pos = route[0]
 				if not person.current_pos == person.start:
 					graph.update_cost(person.prev_pos, person.current_pos, value=-1) # reduces cost of the edge the person is no longer on
