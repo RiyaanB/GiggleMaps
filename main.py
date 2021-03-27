1db916e504ca4a31b178e5ade1ae4c2fab6e8859
NFJ1618 <67336232+NFJ1618@users.noreply.github.com>
2021-03-27 20:30:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import matplotlib.pyplot as plt
 from Graph import Graph
 
 B = Graph('graph.txt')
-print(B.array)
-G = nx.from_numpy_array(B.array, parallel_edges=True, create_using=nx.DiGraph)
-labels = {}
-nodes = list(G.nodes())
-for i in range(len(nodes)):
-    labels[nodes[i]] = B.names[i]
-nx.draw(G, labels=labels, with_labels=True)
+
+G = nx.to_directed(nx.to_networkx_graph(B.edges, multigraph_input=True))
+
+for from_node, to_nodes in B.edges.items():
+    for to_node, value in to_nodes.items():
+        G.edges[(from_node, to_node)]['weight'] = value
+
+pos = nx.spring_layout(G)
+nx.draw(G, with_labels=True, node_color='skyblue', edge_cmap=plt.cm.Blues, pos = pos)
+edge_labels = nx.get_edge_attributes(G,'weight') # key is edge, pls check for your case
+nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels,font_color='red',label_pos=0.2)
 plt.show()
+
 
 
