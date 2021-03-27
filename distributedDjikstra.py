be0646cb4c819964eeac71748430d8f01d565565
NFJ1618 <67336232+NFJ1618@users.noreply.github.com>
2021-03-27 23:42:20 +0530

    Update distributedDjikstra.py

--- distributedDjikstra.py
+++ distributedDjikstra.py
@@ -14,67 +14,67 @@ import time
 from plot_graph import plot_graph
 
 def main():
-	graph = Graph('graph.txt')
+    graph = Graph('graph.txt')
 
-	people = 3
+    people = 3
 
-	with open('start_end.txt') as f:
-		everyone = [Person(row[0], row[1], row[2]) for row in csv.reader(f)]
+    with open('start_end.txt') as f:
+        everyone = [Person(row[0], row[1], row[2]) for row in csv.reader(f)]
 
-	people = everyone.copy()
-	time_taken = 0
-	reached = []
-	tot=0
+    people = everyone.copy()
+    time_taken = 0
+    reached = []
+    tot=0
 
-	while tot<len(people):
+    while tot<len(people):
 
-		graph.time_taken = defaultdict(lambda: 0)
+        graph.time_taken = defaultdict(lambda: 0)
 
-		for idx in range(len(people)):
-			person = people[idx]
-			if not person.reached():
-				
-				route = dijkstra(graph, person.current_pos, person.end)
-				next_pos = route[1][0]
+        for idx in range(len(people)):
+            person = people[idx]
+            if not person.reached():
 
-				graph.update_positions(person.current_pos, next_pos, remove=True)
-				person.path.append(next_pos)
+                route = dijkstra(graph, person.current_pos, person.end)
+                next_pos = route[1][0]
 
-				if not person.current_pos == person.start:
-					graph.update_cost(person.prev_pos, person.current_pos, value=-1) # reduces cost of the edge the person is no longer on
+                graph.update_positions(person.current_pos, next_pos, remove=True)
+                person.path.append(next_pos)
 
-				graph.update_cost(person.current_pos, next_pos, value=1) # increases cost of the edge on which person travels
-				person.prev_pos = person.current_pos
-				
-				person.move(next_pos)
+                if not person.current_pos == person.start:
+                    graph.update_cost(person.prev_pos, person.current_pos, value=-1) # reduces cost of the edge the person is no longer on
 
-				graph.time_taken[(person.prev_pos, person.current_pos)] += 1
+                graph.update_cost(person.current_pos, next_pos, value=1) # increases cost of the edge on which person travels
+                person.prev_pos = person.current_pos
 
-				print(person.path[-2:])
+                person.move(next_pos)
 
-			elif person.already_reached:
-				pass
-			else:
-				print(f"PERSON {person.name} REACHED, PATH:", person.path)
-				reached.append(person)
-				person.already_reached = True
-				tot+=1	
+                graph.time_taken[(person.prev_pos, person.current_pos)] += 1
 
-		try:
-			print(graph.time_taken.values())
-			time = max(graph.time_taken.values())
-			print(f"Time Taken: {time}\n----------")
-			time_taken += time
-		except:
-			pass
+                print(person.path[-2:])
 
-			# take max people at an edge and use that for time taken
-		
-		# plot_graph(graph)
-		# time_taken += max(graph.time_taken.values())
-	
-	print(time_taken)
-	print([person.path for person in everyone])
+            elif person.already_reached:
+                pass
+            else:
+                print(f"PERSON {person.name} REACHED, PATH:", person.path)
+                reached.append(person)
+                person.already_reached = True
+                tot+=1
+
+        try:
+            print(graph.time_taken.values())
+            time = max(graph.time_taken.values())
+            print(f"Time Taken: {time}\n----------")
+            time_taken += time
+        except:
+            pass
+
+            # take max people at an edge and use that for time taken
+
+        # plot_graph(graph)
+        # time_taken += max(graph.time_taken.values())
+
+    print(time_taken)
+    print([person.path for person in everyone])
 
 
 class SpecialMinHeap(Heap):
