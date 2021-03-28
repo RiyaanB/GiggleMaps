dcfa68808133565ec868a5796de6c2c8d960285e
Ayush Agarwal <ayush.ag1301@gmail.com>
2021-03-28 18:01:16 +0530

    google graph data generator

--- google_plot.py
+++ google_plot.py
@@ -3,16 +3,30 @@ import requests
 def main(start, end):
     API = 'AIzaSyCWmcq5BfF4LFha5ZufuEO27ixsl3OpBjs'
     directions_link = "https://maps.googleapis.com/maps/api/directions/json?"
-    start = find_loc(start, API)
-    end = find_loc(end, API)
-    #start = find_loc(start, API)
-    #end = find_loc(end, API)
+    try:
+        start = find_loc(start, API)
+        end = find_loc(end, API)
+        data = get_data(start, end, API=API, directions_link=directions_link, alternatives='true')
+        times = get_time(data)
+        routes = get_routes(data)
+    except Exception:
+        raise RuntimeError('Error in getting data from Google Maps')
 
-    coords = get_route(start, end, API=API, directions_link=directions_link)
-    get_route(coords[3], coords[4], API=API, directions_link=directions_link)
+    
+    fictional_points = [(end[0]+i*1e-6, end[1]+i*1e-6) for i in range(1, len(times) + 1)]
 
+    with open('Google_graph.txt', 'w') as w:
+        modified_start = modify_coord(start)
+        modified_end = modify_coord(end)
 
+        for i in range(len(times)):
+            modified_fictional = modify_coord(fictional_points[i])
+            w.write(','.join( [modified_start, modified_fictional, str(times[i]) ] ) + '\n')
+            w.write(','.join( [modified_fictional, modified_end, '1' ] ) + '\n')
 
+def modify_coord(coords):
+    coords = (str(coord) for coord in coords)
+    return '_'.join(coords)
 
 def find_loc(name, API):
     modified_name = '+'.join(name.replace(',', '').split(sep=' '))
@@ -23,28 +37,62 @@ def find_loc(name, API):
 
     return lat, lng
 
-
-def get_route(start, end, API, directions_link, alternatives='false'):
-    link = directions_link + f'origin={start[0]},{start[1]}&destination={end[0]},{end[1]}&key={API}&mode=driving&alternatives={alternatives}&departure_time=now&traffic_model=pessimistic'
-    print(link)
+def get_data(start, end, API, directions_link, alternatives='false'):
+    link = directions_link + f'origin={start[0]},{start[1]}&destination={end[0]},{end[1]}&key={API}&mode=driving&alternatives={alternatives}'
+    #print(link)
     response = requests.get(link)
-
     resp_json_payload = response.json()
-    #print(resp_json_payload)
-    coords = []
 
-    for dot in resp_json_payload['routes'][0]['legs'][0]['steps']:
-        lat_end = dot['end_location']['lat']
-        lng_end = dot['end_location']['lng']
-        lat_start = dot['start_location']['lat']
-        lng_start = dot['start_location']['lng']
-        coords.append((lat_end, lng_end))
-        coords.append((lat_start, lng_start))
+    return resp_json_payload
 
-    return coords
+def get_time(resp_json_payload):
+    return [route['legs'][0]['duration']['value'] for route in resp_json_payload['routes']]
+        
 
-main('Ashok Gardens, Mumbai', 'High Street Phoenix, Mumbai')
+def get_routes(resp_json_payload):
+    all_coords = []
+    for i in range(len(resp_json_payload['routes'])):
+        path_coords = []
+        for dot in resp_json_payload['routes'][i]['legs'][0]['steps']:
+            lat_end = dot['end_location']['lat']
+            lng_end = dot['end_location']['lng']
+            lat_start = dot['start_location']['lat']
+            lng_start = dot['start_location']['lng']
+            path_coords.append((lat_end, lng_end))
+            path_coords.append((lat_start, lng_start))
+        
+        all_coords.append(path_coords)
+        
+    return all_coords
+
+
+main('Ashok Gardens, Mumbai', 'Dhirubhai Ambani International School, Mumbai')
 
 
 
 # https://maps.googleapis.com/maps/api/directions/json?origin=18.9953464,72.848134&destination=18.994744,72.82459930000002&key=AIzaSyCWmcq5BfF4LFha5ZufuEO27ixsl3OpBjs&mode=driving
+
+'''
+    r = set(coord for coord in routes[0])
+    r1 = set(coord for coord in routes[1])
+    r2 = set(coord for coord in routes[2])
+
+    r3 = r.union(r1).union(r2)
+
+    print(len(r3))
+    exit()
+    r = set()
+    for route in routes:
+        for coord in route:
+            r.add(str(coord))
+        print(len(r))
+
+    print(len(r))
+
+    for route in routes:
+        print(len(route))
+
+    exit()
+    '''
+
+#&departure_time=now&traffic_model=pessimistic
