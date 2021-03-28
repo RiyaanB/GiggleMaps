b85effd899eaa42d72c15c2d73fd18e2792631db
twixupmysleeve <pratham.mehta001@gmail.com>
2021-03-28 20:26:11 +0530

    implemented basic example

--- trial.py
+++ trial.py
@@ -1,6 +1,25 @@
-from distributedDjikstra import dijkstra
+from distributedDjikstra import simulator, google_maps, giggle_maps
 from Graph import Graph
+from main import draw
+import csv
+from Person import Person
+import matplotlib.pyplot as plt
 
-graph = Graph('test_graphs/graph2.csv')
 
-print(dijkstra(graph, '1', '3'))
+graph = Graph('test_graphs/graph3.txt')
+graph2 = Graph('test_graphs/graph3.txt')
+
+people = []
+people2 = []
+
+with open('test_persons/people_start_end_same.csv', 'r') as file:
+		reader = csv.reader(file)
+		for row in reader:
+			people.append(Person(row[0], row[1]))
+			people2.append(Person(row[0], row[1]))
+
+
+print(simulator(graph, google_maps(graph, people)))
+print(simulator(graph2, giggle_maps(graph2, people2)))
+draw(graph2)
+
